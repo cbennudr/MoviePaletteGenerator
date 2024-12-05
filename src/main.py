99452bff4a01011b8282091072c6386f6dff5b32
cbennudr <cbennudriti@hotmail.com>
2024-12-04 19:43:37 -0600

    Added functionality for various sized images and changing the frames used

--- src/main.py
+++ src/main.py
@@ -9,63 +9,53 @@ import multiprocessing as mp
 from utils import *
 
 
-def create_pallete_img(colors:list, width:int, height:int) -> np.array:
+def stitch_colors(colors:list, width:int, height:int) -> np.array:
+    """Function to go through a list of colors and stitch strips of each color into an image"""
     
     # Adjust size to fit frames correctly
     num_colors = len(colors)
-    width_of_each_strip = ceil(width/num_colors)
-    # pallete_width = width_of_each_color_strip * num_colors
-    # img_size = (pallete_width, height)
+    width_of_each_strip = 1#ceil(width/num_colors)
 
-    # Loop through the colors, create a strip for each, and add it to the pallete image
-    subpallets = []
-    with tqdm(total=len(colors), desc="Creating pallete image") as pbar:
-        i=1
+    # Loop through the colors, create a strip for each, then combine all the strips into the final image
+    strips = []
+    with tqdm(total=len(colors), desc="Creating palette image") as pbar:
         for color in colors:
             cur_strip = np.full((height,width_of_each_strip,3), color)
-            if i==1: pallete = cur_strip.copy() # on the first strip for a new subpalette, just set the palette to be the strip
-            else: pallete = np.hstack([pallete, cur_strip]) 
-            # Every 500 iterations, start creating a new sub-pallete or else the hstack operation starts taking way too long
-            if i % 500 == 0:
-                subpallets.append(pallete)
-                i=0
-            i+=1
+            strips.append(cur_strip)
             pbar.update(1)
-        if i!=1: subpallets.append(pallete) # if the number of frames was not a multiple of 500, we need to hstack the subpalette that was still being created
 
-    print("Combining sub-pallets")
-    pallete = np.hstack(subpallets)
+    print("Combining strips")
+    palette = np.hstack(strips)
 
-
-    return pallete
+    return palette
 
 
-def create_pallete(video_dir:str, output_dir:str, output_filename:str, pallete_width:int, pallete_height:int, display_frames:bool, color_selection_method:str):
-    # Check if the video exists
-    if not os.path.exists(video_dir):
-        raise Exception("Provided path to video does not reference an existing file")
-    
-    # Create output dir
-    os.makedirs(output_dir, exist_ok=True)
+def create_palette_img(video_dir:str, palette_width:int, palette_height:int, display_frames:bool, color_selection_method:str, use_every_n_frames:int):
+    """Function to create a palette image from frames in a video"""
 
     # Open the video
     cap = cv2.VideoCapture(video_dir)
     if not cap.isOpened(): 
         raise Exception("Unable to open VideoCapture")
 
+    # Calculate frames to skip to fit set width 
+    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    if (use_every_n_frames is not None):    every_n_frames = use_every_n_frames
+    elif (palette_width >= total_frames):   every_n_frames = 0
+    else:                                   every_n_frames = int(total_frames/palette_width)
+    print("Using every frame") if every_n_frames==0 else print(f"Using every {every_n_frames} frames")
+    
     # Get dominant (or average) color from each frame
-    colors = get_colors_from_frames(cap, 1, color_selection_method=color_selection_method, display_frames=display_frames)
+    colors = get_colors_from_frames(cap, every_n_frames, color_selection_method=color_selection_method, display_frames=display_frames)
 
-    # Create the pallete image from the colors array
-    pallete_img = create_pallete_img(colors, pallete_width, pallete_height)
-
-    # Save the image
-    file_path = os.path.join(output_dir, output_filename) 
-    cv2.imwrite(file_path, pallete_img)
+    # Create the palette image from the colors array
+    palette_img = stitch_colors(colors, palette_width, palette_height)
+    return palette_img
+    
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="")
+    parser = argparse.ArgumentParser(description="Program to create a palette image using the average (or dominant) color from frames in a video")
     ######################### Required args
     parser.add_argument('--video', '-v', required=True)
     parser.add_argument('--output_dir', '-o', required=True)
@@ -73,12 +63,23 @@ if __name__ == "__main__":
     ######################### Program behavior args
     parser.add_argument('--display', '-d', action='store_true')
     parser.add_argument('--color_selection_method', '-c', choices=['average', 'dominant'], default='average')
+    parser.add_argument('--use_every_n_frames', type=int,                   help='How many frames to skip between color grabs - overrides the "width" arg. Using a value of "0" results in every frame being used')
     ######################### Output args
-    parser.add_argument('--width', '-w', type=int, default=1920,        help='The width of the resulting pallete (the final result will be slightly different to adjust for the number of frames)')
-    parser.add_argument('--height', '-g', type=int, default=1080,       help='The height of the resulting pallete')
-    parser.add_argument('--border_size', type=int, default=0,           help="The size of the border that is added to the resulting image (0 = no border)")
-    parser.add_argument('--border_color', default='(255,255,255)',      help='The color of the border in the resuluting image (in BGR format) - eg "(0,0,255)"')
+    parser.add_argument('--width', '-w', type=int, default=2560,        help='The width of the resulting palette')
+    parser.add_argument('--height', '-g', type=int, default=1080,       help='The height of the resulting palette')
+    parser.add_argument('--border_size', type=int, default=0,           help="The size of the border that is added to the final image (0 = no border)")
+    parser.add_argument('--border_color', default='(255,255,255)',      help='The color of the border in the final image (in BGR format) - eg "(0,0,255)"')
 
     args = parser.parse_args()
 
-    create_pallete(args.video, args.output_dir, args.output_filename, args.width, args.height, args.display, args.color_selection_method)
+
+    # Check if the video exists
+    if not os.path.exists(args.video):
+        raise Exception("Provided path to video does not reference an existing file")
+    # Create output dir
+    os.makedirs(args.output_dir, exist_ok=True)
+    # Create the palette
+    palette_img = create_palette_img(args.video, args.width, args.height, args.display, args.color_selection_method, args.use_every_n_frames)
+    # Save the image
+    file_path = os.path.join(args.output_dir, args.output_filename) 
+    cv2.imwrite(file_path, palette_img)
--- src/utils.py
+++ src/utils.py
@@ -3,12 +3,16 @@ import numpy as np
 from tqdm import tqdm
 
 def get_avg_color_from_frame(frame:np.array) -> list[3]:
+    """Function to get the average color in a frame"""
+
     ### From `https://stackoverflow.com/questions/43111029/how-to-find-the-average-colour-of-an-image-in-python-with-opencv`
     average = frame.mean(axis=0).mean(axis=0)
 
     return average
 
 def get_dominant_color_from_frame(frame:np.array) -> list[3]:
+    """Function to get the dominant color in a frame"""
+    
     ### From `https://stackoverflow.com/questions/43111029/how-to-find-the-average-colour-of-an-image-in-python-with-opencv`
     pixels = np.float32(frame.reshape(-1, 3))
     n_colors = 5
@@ -22,36 +26,38 @@ def get_dominant_color_from_frame(frame:np.array) -> list[3]:
 
 
 def get_colors_from_frames(cap, every_n_frames:int, color_selection_method:str, display_frames:bool) -> list:
-    """
-    get_colors_from_frames
-
-    Args:
-        cap: a cv2.VideoCapture instance
-        every_n_frames: the number of frames between color grabs (eg a value of 1 results in every frame having a strip in the resulting pallete while a value of 2 will use every other frame)
-        color_selection_method: a string in ["average", "dominant"] that chooses how the color to represent a frame will be chosen
-
-    Returns:
-        list: a list containing the BGR tuples in chronological order for the selected colors from each frame
-    """
+    """Function to loop through the frames in a cv2.VideoCapture and pull a color from them"""
 
     colors = []
     total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT)) # for progress bar
     running = True
     with tqdm(total=total_frames, desc="Extracting colors from frames") as pbar:
+        frames_since_last_grab = 0
         while running:
+
+            # Read next frame and check if videocapture is done
             ret, frame = cap.read()
             if not ret: 
                 running = False
                 break
-            pbar.update(every_n_frames)
+
+            # If the set number of frames has passed since the last grab, reset the counter and proceed with the rest of the function
+            if frames_since_last_grab == every_n_frames:
+                frames_since_last_grab = 0
+            # If skipping frames, increment counter and go to next iteration
+            else: 
+                frames_since_last_grab += 1
+                continue
+
+            # Update the progress bar
+            pbar.update(every_n_frames+1)
 
             # Get the color for the current frame using whatever method was selected
             if color_selection_method=='average':
                 frame_color = get_avg_color_from_frame(frame)
-            elif color_selection_method=='majority':
+            elif color_selection_method=='dominant':
                 frame_color = get_dominant_color_from_frame(frame)
-            else:
-                raise ValueError("Invalid color selection method")
+            else: raise ValueError("Invalid color selection method")
             
             colors.append(frame_color)
 
